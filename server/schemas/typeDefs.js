50dc39681655f7c7696b4b49481fe7423f131aa3
Jaysahhn <jasonmason96@yahoo.com>
2024-01-30 22:57:04 -0600

    updated packages

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,4 @@
-// import setup for Apollo Server with Express
-const { gql } = require('@apollo/server');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
     type User {
